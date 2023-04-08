2653d7ff455cd5f4cf4b66b531f03621cb6f0b1f
SatellaSoft <satellasoft@gmail.com>
2023-04-08 11:18:29 -0300

    feat: push dashboard script

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuLateral = document.querySelector('#menu-lateral')
+const navToggler = document.querySelector('.navbar-toggler')
+
+navToggler.addEventListener('click', () => {
+    menuLateral.classList.toggle('show')
+})
